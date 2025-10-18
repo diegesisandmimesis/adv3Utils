5387e82eef5d46b58ba653d708b5a42a6bf0bd68
Stephen P. Berry <spb@mothman.meshuggeneh.net>
2025-10-17 19:59:02 -0700

    Added OrdinalThing

--- adv3Utils.h
+++ adv3Utils.h
@@ -49,6 +49,8 @@
 
 #define isSmallWord(v) (isString(v) && (_sw.smallWords.indexOf(v.toLower()) != nil))
 
+OrdinalThing template 'vocabWords' 'name' +ordinalNumber 'ordinalVocab'? @location? "desc"?;
+
 
 #ifdef __DEBUG
 #define gDebugObj(obj) (_debugObject(obj))
